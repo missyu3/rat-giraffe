dfdfcc420c81e00c1451a17bbdd6b86822848866
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-26 18:13:27 +0900

    Add Propertyテーブルを作成

---
+++ db/migrate/20200326085424_create_properties.rb
@@ -0,0 +1,12 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :name , :null => false
+      t.integer :rent , :null => false
+      t.text :address , :null => false
+      t.integer :age , :null => false
+      t.text :remark , :null => false
+      t.timestamps
+    end
+  end
+end
