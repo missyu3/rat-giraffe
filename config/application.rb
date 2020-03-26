e7f73bbfca4b76d7d05e5af98568013efa38e689
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-26 17:30:31 +0900

    Add 不要なファイルを作成しないよう設定を追加

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,12 @@ module DevExam
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.load_defaults 5.1
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.stylesheets false
+      g.test_framework false
+    end
   end
 end
