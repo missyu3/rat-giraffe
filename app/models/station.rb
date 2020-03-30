8f5274e65c80af67be255e43fa510e7a222387b8
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-03-30 11:24:29 +0900

    ADD　編集機能追加

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,17 @@
 class Station < ApplicationRecord
   belongs_to :property
-  with_options presence: true do
-    validates :name
-    validates :route
-    validates :walk_minutes
+  with_options presence: true,if: :input_item?  do |item|
+    item.validates :name
+    item.validates :route
+    item.validates :walk_minutes
   end
+
+  def input_item?
+    if name.blank? && route.blank? && walk_minutes.blank?
+      false
+    else
+      name.blank? || route.blank? || walk_minutes.blank?
+    end      
+  end
+
 end
